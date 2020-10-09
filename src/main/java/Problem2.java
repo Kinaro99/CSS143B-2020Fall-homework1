45474b53c40192720c201fe74ea87dd59d693237
Kinaro99 <abbie687@uw.edu>
2020-10-08 22:21:07 -0700

    Problem 2 and GeekforGeek link

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -3,5 +3,22 @@ public class Problem2 {
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
         // Add your solution
+        boolean isNotSorted = true;
+        int length = data.length;
+        for (int i = 0; i < length && isNotSorted; i++) {
+            isNotSorted = false;
+            for (int j = 0; j < length - i - 1; j++) {
+                if (data[j] > data[j + 1]) {
+                    swapInteger(data, j);
+                    isNotSorted = true;
+                }
+            }
+        }
+    }
+
+    public static void swapInteger(int[] data, int j) {
+        int placeHolder = data[j];
+        data[j] = data[j + 1];
+        data[j + 1] = placeHolder;
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+/*Tests in ascending order*/
 
 public class Problem2Test {
 
@@ -8,7 +8,24 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
+        //assertTrue(false);
         // add your tests here
+
+        Problem2 test = new Problem2();
+        int[] testOne = {3,2,1};
+        int[] testTwo = {2,3,1};
+        int[] testThree = {9,5,2};
+        int[] testFour = {4,4,4};
+        int[] testFive = {0,4,4};
+
+        test.bubbleSort(testOne);
+        test.bubbleSort(testTwo);
+        test.bubbleSort(testThree);
+        test.bubbleSort(testFour);
+        test.bubbleSort(testFive);
+
     }
 }
+
+// Hii. I used geeksforgeeks to figure out how to do my test. I kept getting an error when trying to call the bubbleSort method in class problem2 and couldn't figure out why.
+// Here is the link:https://www.geeksforgeeks.org/bubble-sort/ I used the "driver method test above" section in java
